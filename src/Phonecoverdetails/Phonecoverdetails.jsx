0921b79a5a2ed35ef3eb3d3c79dcb20971e81566
Hisham Hany <hishammmmmmbeh@gmail.com>
2025-10-21 01:02:24 +0300

    YrarbFinish

--- src/Phonecoverdetails/Phonecoverdetails.jsx
+++ src/Phonecoverdetails/Phonecoverdetails.jsx
@@ -1274,7 +1274,7 @@ const handleBuyNow = () => {
                 setSelectedBrand(e.target.value);
                 setSelectedModel("");
               }}
-              className="w-full border rounded-lg p-3 focus:ring-2 focus:ring-green-400 outline-none sm:overflow-y-scroll"
+              className="w-full border rounded-lg p-3 focus:ring-2 focus:ring-green-400 outline-nonemax-h-48 overflow-y-auto sm:max-h-60"
             >
               <option value="">-- Choose Brand --</option>
               {Object.keys(phoneData).map((brand) => (
@@ -1294,7 +1294,7 @@ const handleBuyNow = () => {
               <select
                 value={selectedModel}
                 onChange={(e) => setSelectedModel(e.target.value)}
-                className="w-full border rounded-lg p-3 focus:ring-2 focus:ring-green-400 outline-none sm:overflow-y-scroll"
+                className="w-full border rounded-lg p-3 focus:ring-2 focus:ring-green-400 outline-none max-h-48 overflow-y-auto sm:max-h-60"
               >
                 <option value="">-- Choose Model --</option>
                 {phoneData[selectedBrand].map((model) => (
